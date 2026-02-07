9782883ab8686de7c69c452ce4427af165b276ed
Allisson Alves Batista Nunes <allissonabn@gmail.com>
2026-02-07 19:15:44 -0300

    Update tests and persistence logic; migrate to PostgreSQL
    Customer test data was enhanced with emails and randomized data generation. Improved transactional handling in persistence methods. Replaced the H2 database with PostgreSQL, adjusted configuration, and updated test setup accordingly.

--- .docker/postgres/init-user-db.sh
+++ .docker/postgres/init-user-db.sh
@@ -3,5 +3,7 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE DATABASE ordering;
+	CREATE DATABASE ordering_test;
 	CREATE DATABASE billing;
+	CREATE DATABASE billing_test;
 EOSQL
